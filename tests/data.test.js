60e9e6a3e1f7f2ddb628cedec8630e1621bf6f0f
IUT-Toulon-Maxence-Jamme <78029916+IUT-Toulon-Maxence-Jamme@users.noreply.github.com>
2025-02-14 14:27:14 +0100

    correction des tests

--- tests/data.test.js
+++ tests/data.test.js
@@ -1,16 +1,18 @@
 const request = require('supertest');
 const app = require('../app');
 
-describe('Test de la route plane de l\'application', () => {
+describe('Test de la route data de l\'application', () => {
 
 
-    test("GET /plane doit retourner 200 et contient une compagnie & un vol", async () => {
-        const response = await request(app).get('/plane');
+    test("GET /data doit retourner 200 et contient une compagnie & un vol", async () => {
+        const response = await request(app).get('/data');
         expect(response.statusCode).toBe(200);
         expect(response.body).toHaveProperty('airline_flight_number');
         expect(typeof response.body.airline_flight_number).toBe('string');
         expect(response.body).toHaveProperty('airline_name');
         expect(typeof response.body.airline_name).toBe('string');
+        expect(response.body).toHaveProperty('full_name');
+        expect(typeof response.body.full_name).toBe('string');
     });
 
 });
